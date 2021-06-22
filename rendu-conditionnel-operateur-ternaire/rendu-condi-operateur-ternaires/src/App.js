ab65ce0743943568f7034a9e030a70b32b027316
Chardon François <francoischardon.contact@gmail.com>
2021-06-22 09:27:51 +0200

    update

---
+++ rendu-conditionnel-operateur-ternaire/rendu-condi-operateur-ternaires/src/App.js
@@ -0,0 +1,12 @@
+import Item from './components/Item/Item';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello depuis App.</h1>
+      <Item />
+    </div>
+  );
+}
+
+export default App;
---
+++ rendu-conditionnel-operateur-ternaire/rendu-condi-operateur-ternaires/src/components/Item/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './item.css';
+
+function Item() {
+  return (
+    <div>
+      <h1 className="title-item">hello depuis Item.</h1>
+    </div>
+  );
+}
+export default Item;
